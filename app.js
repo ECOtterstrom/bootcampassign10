028e9f5613c819f73f79f340d5fe757d4387b2da
Erin Otterstrom <hapiotter@gmail.com>
2020-05-09 18:50:29 -0600

    tests passing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const employee = require("./lib/employee");
+const Employee = require("./lib/employee");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
