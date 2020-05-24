989a4be3ca89ca0ec09e5253b3c8e08f5d62ab67
Erin Otterstrom <hapiotter@gmail.com>
2020-05-23 19:14:03 -0600

    project complete

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const employeeArray =[];
+const employeeArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -21,28 +21,54 @@ function createManager() {
         {
             type: "input",
             name: "name",
-            message: "What is the employee's name?"
+            message: "What is the managers's name?"
         },
         {
             type: "input",
             name: "id",
-            message: "What is the employee's id?"
+            message: "What is the manager's id?"
         },
         {
             type: "input",
-            id: "email",
-            message: "What is the employee's email address?"
+            name: "email",
+            message: "What is the managers's email address?"
         },
         {
             type: "input",
-            id: "number",
-            message: "What is the employee's office number?"
+            name: "number",
+            message: "What is the managers's office number?"
         }
-    ]).then(function(data){
+    ]).then(function (data) {
         var employee = new Manager(data.name, data.id, data.email, data.number);
         employeeArray.push(employee);
+        newEmployee();
     })
 };
+
+function newEmployee() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Do you want to create another employee?",
+            choices: ["engineer", "intern", "I'm done. Build the team."]
+        }
+    ]).then(function (choices) {
+        var choice = choices.choice
+        if (choice === "engineer") {
+            createEngineer();
+        } else if (choice === "intern") {
+            createIntern();
+        } else {
+            buildTeam();
+        }
+    })
+}
+
+function buildTeam() {
+    var output = render(employeeArray);
+    fs.writeFileSync( outputPath, output )
+}
 
 function createEngineer() {
     return inquirer.prompt([
@@ -66,9 +92,10 @@ function createEngineer() {
             name: "github",
             message: "What is the employee's github username?"
         }
-    ]).then(function(data){
+    ]).then(function (data) {
         var employee = new Engineer(data.name, data.id, data.email, data.github);
         employeeArray.push(employee);
+        newEmployee();
     })
 };
 
@@ -86,32 +113,35 @@ function createIntern() {
         },
         {
             type: "input",
-            id: "email",
+            name: "email",
             message: "What is the employee's email address?"
         },
         {
             type: "input",
-            id: "school",
+            name: "school",
             message: "What school is the employee attending?"
         }
-    ]).then(function(data){
+    ]).then(function (data) {
         var employee = new Intern(data.name, data.id, data.email, data.school);
         employeeArray.push(employee);
         console.log(employeeArray);
+        newEmployee();
     })
 };
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
- var output = render(employeeArray); 
+    createManager();
+ //var output = render(employeeArray);
+ //console.log(output)
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-**need fs.writeFile - see week 9
+        //**need fs.writeFile - see week 9
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
